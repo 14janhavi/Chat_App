9516bab5aff4a68b263e9fdf6441b43b0768b2e9
14janhavi <163433136+14janhavi@users.noreply.github.com>
2026-01-01 21:07:25 +0530

    Fix Netlify blank page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/Chat_App">       {/* <-- Wrap App with BrowserRouter */}
+    <BrowserRouter basename="/">       {/* <-- Wrap App with BrowserRouter */}
       <App />
     </BrowserRouter>
   </StrictMode>,
