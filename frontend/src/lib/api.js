bc388597375cd732b828b809c0d25e634dd3eb1b
14janhavi <163433136+14janhavi@users.noreply.github.com>
2026-01-02 14:10:07 +0530

    Fix CORS, API URL, and Netlify routing

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,9 +1,12 @@
 // lib/api.js
 import axios from "axios";
 
+const API = "https://chat-app-pqax.onrender.com";
+
 export const getStreamToken = async () => {
-  const res = await axios.get("http://localhost:5001/api/video/token", {
+  const res = await axios.get(`${API}/api/video/token`, {
     withCredentials: true,
   });
   return res.data;
 };
+
