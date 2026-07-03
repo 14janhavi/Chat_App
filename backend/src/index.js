0e8b0bf3f03d201c1c5acee31e6a3ce576da300c
14janhavi <163433136+14janhavi@users.noreply.github.com>
2026-07-03 15:12:19 +0530

    Increase request size limit

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,7 +20,12 @@ const __dirname = path.resolve();
 
 // ================= MIDDLEWARE =================
 app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(
+  express.urlencoded({
+    extended: true,
+    limit: "50mb",
+  })
+);
 app.use(cookieParser());
 
 app.use(
