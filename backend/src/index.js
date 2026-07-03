a02082875e9563a648b9ba968719dd994d7dd711
14janhavi <163433136+14janhavi@users.noreply.github.com>
2026-07-03 15:05:11 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -19,7 +19,8 @@ const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
 // ================= MIDDLEWARE =================
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser());
 
 app.use(
